1afdc44b9d2a6a6b1e427d82b1d19cf153ca08ab
vwjaya32 <vinsen.wijaya@ui.ac.id>
2022-09-12 23:53:08 +0700

    first attempt

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_item': data_item_katalog,
+        'nama': 'Vinsen Wijaya',
+        'student_id': '2106637776'
+    }
+    return render(request, "katalog.html", context)
